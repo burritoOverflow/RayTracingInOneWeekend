7b3435a1f562aabff3a9a6f716585d0de62ed50f
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2023-08-20 15:53:41 -0400

    Initial additions of operator overloading for Vector3

--- include/vec3.h
+++ include/vec3.h
@@ -13,6 +13,30 @@ class Vector3 {
     double y() const { return e_[1]; }
     double z() const { return e_[2]; }
 
+    Vector3 operator-() const { return Vector3(-e_[0], -e_[1], -e_[2]); }
+    double operator[](int i) const { return e_[i]; }
+    double& operator[](int i) { return e_[i]; }
+
+    Vector3& operator+=(const Vector3& v) {
+        e_[0] += v.x();
+        e_[1] += v.y();
+        e_[2] += v.z();
+        return *this;
+    }
+
+    Vector3& operator*=(double d) {
+        e_[0] *= d;
+        e_[1] *= d;
+        e_[2] *= d;
+        return *this;
+    }
+
+    Vector3& operator/=(double d) { return *this *= 1 / d; }
+
+    double length() const { return std::sqrt(length_squared()); }
+
+    double length_squared() const { return e_[0] * e_[0] + e_[1] * e_[1] + e_[2] * e_[2]; }
+
    private:
     double e_[3];
 };

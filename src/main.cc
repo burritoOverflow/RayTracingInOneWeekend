3d566bc1e1b57203d366222aaaacf0e69eff58cc
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2023-08-21 21:16:15 -0400

    Refactor constants to new header

--- src/main.cc
+++ src/main.cc
@@ -4,54 +4,15 @@
 #include <iostream>
 #include <sstream>
 #include "color.h"
+#include "config.h"
 #include "ray.h"
 #include "vec3.h"
-
-// see rationale in section 4.2
-
-// Image
-
-const auto ASPECT_RATIO = 16.0 / 9.0;
-const int IMAGE_WIDTH = 800;
-const int IMAGE_HEIGHT = static_cast<int>(IMAGE_WIDTH / ASPECT_RATIO);
-const auto VIEWPORT_HEIGHT = 2.0;
-const auto VIEWPORT_WIDTH = VIEWPORT_HEIGHT * (static_cast<double>(IMAGE_WIDTH) / IMAGE_HEIGHT);
-
-// Camera
-
-const auto FOCAL_LENGTH = 1.0;
-const auto CAMERA_CENTER = Point3(0, 0, 0);
-
-// Viewport vectors (see section 4.2)
-
-// v_u - vector from the left to the right edge
-const auto VIEWPORT_U = Vector3(VIEWPORT_WIDTH, 0, 0);
-
-// v_v - vector from the upper edge to the lower edge
-// y increases down the image
-const auto VIEWPORT_V = Vector3(0, -VIEWPORT_HEIGHT, 0);
-
-// Calculate the horizontal and vertical delta vectors from pixel to pixel.
-
-// delta_u - distance between pixels on the x-axis
-const auto PIXEL_DELTA_U = VIEWPORT_U / IMAGE_WIDTH;
-
-// delta_v - distance between pixels on the y-axis
-const auto PIXEL_DELTA_V = VIEWPORT_V / IMAGE_HEIGHT;
-
-// location of the upper left pixel - x = 0, y = 0
-const auto VIEWPORT_UPPER_LEFT =
-    CAMERA_CENTER - Vector3(0, 0, FOCAL_LENGTH) - VIEWPORT_U / 2 - VIEWPORT_V / 2;
-
-const auto PIXEL00_LOC = VIEWPORT_UPPER_LEFT + 0.5 * (PIXEL_DELTA_U + PIXEL_DELTA_V);
-
-const std::string DIRNAME = "images";
 
 using time_point = std::chrono::steady_clock::time_point;
 
 void CreateImageOutdir() {
-    if (!std::filesystem::exists(DIRNAME)) {
-        std::filesystem::create_directory(DIRNAME);
+    if (!std::filesystem::exists(config::DIRNAME)) {
+        std::filesystem::create_directory(config::DIRNAME);
     }
 }
 
@@ -94,23 +55,24 @@ int main() {
 
     const std::string date_fmt_str = "%m-%d-%Y_%H-%M-%S";
     const auto image_out_filename = GetCurrentDateStr(std::move(date_fmt_str)) + "image.ppm";
-    auto image_path = std::filesystem::path(DIRNAME) / image_out_filename;
+    auto image_path = std::filesystem::path(config::DIRNAME) / image_out_filename;
 
     std::ofstream out_stream(image_path.string().c_str());
-    out_stream << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << "\n255\n";
+    out_stream << "P3\n" << config::IMAGE_WIDTH << ' ' << config::IMAGE_HEIGHT << "\n255\n";
 
     const auto start_time = std::chrono::steady_clock::now();
 
-    for (int j = 0; j < IMAGE_HEIGHT; ++j) {
-        std::clog << "\r" << GetLogPreamble() << "  Scanlines remaining: " << (IMAGE_HEIGHT - j)
-                  << ' ' << std::flush;
+    for (int j = 0; j < config::IMAGE_HEIGHT; ++j) {
+        std::clog << "\r" << GetLogPreamble()
+                  << "  Scanlines remaining: " << (config::IMAGE_HEIGHT - j) << ' ' << std::flush;
 
-        for (int i = 0; i < IMAGE_WIDTH; ++i) {
-            const auto pixel_center = PIXEL00_LOC + (i * PIXEL_DELTA_U) + (j * PIXEL_DELTA_V);
-            const auto ray_direction = pixel_center - CAMERA_CENTER;
+        for (int i = 0; i < config::IMAGE_WIDTH; ++i) {
+            const auto pixel_center =
+                config::PIXEL00_LOC + (i * config::PIXEL_DELTA_U) + (j * config::PIXEL_DELTA_V);
+            const auto ray_direction = pixel_center - config::CAMERA_CENTER;
 
-            Ray ray{CAMERA_CENTER, ray_direction};
-            Color pixel_color = RayColor(ray);
+            Ray ray{config::CAMERA_CENTER, ray_direction};
+            const Color pixel_color = RayColor(ray);
 
             WriteColor(out_stream, pixel_color);
         }

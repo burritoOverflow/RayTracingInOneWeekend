543d6198e327ae04b0ab20186aa83e1403afefbd
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2023-09-14 23:30:18 -0400

    Log number of elapsed minutes; format seconds properly

--- src/config.cc
+++ src/config.cc
@@ -29,17 +29,24 @@ void config::CreateImageOutdir() {
 void config::LogDuration(const std::chrono::steady_clock::time_point start_time,
                          const std::chrono::steady_clock::time_point end_time) {
     const auto total_time = end_time - start_time;
-    const auto elapsed_ms =
-        std::chrono::duration_cast<std::chrono::milliseconds>(total_time).count();
+
+    const auto elapsed_min = std::chrono::duration_cast<std::chrono::minutes>(total_time).count();
+    const auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(total_time).count();
     const auto elapsed_s = std::chrono::duration_cast<std::chrono::seconds>(total_time).count();
 
     long output_ms;
+    long output_s = elapsed_s;
+
+    if (elapsed_min > 0) {
+        output_s -= elapsed_min * 60;
+    }
+
     if (elapsed_s > 1l) {
         output_ms = elapsed_ms % (elapsed_s * static_cast<long>(pow(10, 3)));
     } else {
         output_ms = elapsed_ms;
     }
 
-    std::clog << "\rDuration: " << elapsed_s << "s " << output_ms << " ms "
+    std::clog << "\rDuration: " << elapsed_min << "m " << output_s << "s " << output_ms << " ms "
               << "\n";
 }
